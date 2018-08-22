7db6732bc771c5ecf258e68cfbe6ae22492b30d2
fddata <fraserjdouglas@gmail.com>
2018-08-22 20:21:19 +0100

    adds getUNiqueModesOfTransport test pass

--- models/traveller.js
+++ models/traveller.js
@@ -39,8 +39,19 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  //sauce: https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
 
+  // More than one way to do this. We can create an array from a set.
+ //Sets contain unique values only. ... is the 'spread' operator:
+  return  [...new Set(this.getModesOfTransport())];
+
+//Or we can find the first (and only the first) index of each value,
+//then filter by these unique indexes:
+  // return this.getModesOfTransport().filter((value, index, array) => {
+  //   return array.indexOf(value) === index;
+  // });
 };
+
 
 
 module.exports = Traveller;
